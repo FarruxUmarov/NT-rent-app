8fc1756f9e07f040ee6257251b8cae117c781a3e
Farrukh <adler4313@gmail.com>
2024-10-29 20:02:05 +0500

    chore:Status table

--- database/migrations/2024_10_29_144245_create_statuses_table.php
+++ database/migrations/2024_10_29_144245_create_statuses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('statuses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
         });
     }
